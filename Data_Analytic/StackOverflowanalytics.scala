26be340298d1d1ff0af95555ca2e4381eb08dabf
Monil Shah <msmonilshah007@live.com>
2016-12-08 18:34:09 -0500

    changed path to files

--- Data_Analytic/StackOverflowanalytics.scala
+++ Data_Analytic/StackOverflowanalytics.scala
@@ -1,6 +1,6 @@
-val rddgit =  sc.textFile("/user/mds747/Githublang")
+val rddgit =  sc.textFile("/user/mds747/project/Githublang")
 val rddgit2array = sc.broadcast(rddgit.collect())
-val data = sc.textFile("/user/mds747/cleaning_output_final.txt")
+val data = sc.textFile("/user/mds747/project/cleaning_output_final.txt")
 rddgit2array.value.foreach{ x=> {
 			val lang = data.filter(line=>line.contains(x))
 			lang.saveAsTextFile(s"/user/mds747/project/$x")
