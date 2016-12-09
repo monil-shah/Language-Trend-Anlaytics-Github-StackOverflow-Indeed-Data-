df0c0b54efd1db68cab0c1ce7b1aba8eb50f3d67
Yash Balar <ydb219@nyu.edu>
2016-12-09 13:04:45 -0500

    Add files via upload

---
+++ Data_Analytic/IndeedAnalytic2.scala
@@ -0,0 +1,16 @@
+val indeedData = sc.textFile("/dataset.txt")
+val githubOutput = sc.textFile("/output.txt")
+
+val githubD = sc.broadcast(githubOutput.collect())
+
+val result = githubD.value.map( line => {
+	val lang = indeedData.filter(x => x.contains(line))
+	val cout = lang.count
+	(x, cout)
+	})
+	
+import scala.collection.immutable.ListMap
+
+val mResult = ListMap(result.toSeq.sortWith(_._2 > _._2):*)).toSeq
+val mapResult = sc.parallelize(mResult)
+mapResult.saveAsTextFile("/Project/OverallCount")
