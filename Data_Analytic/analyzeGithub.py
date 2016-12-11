f48cecea7a5ddeaa6b7cf772d44b2ba99877ac35
Tabish Sada <sada.tabish@gmail.com>
2016-12-11 14:09:47 -0500

    Update analytic code

--- Data_Analytic/analyzeGithub.py
+++ Data_Analytic/analyzeGithub.py
@@ -4,4 +4,8 @@ inputsRDD = sc.textFile("input")
 
 counts = inputsRDD.flatMap(lambda line: list(json.loads(line)["event_repo_languages"].keys())).map(lambda word: (word, 1)).reduceByKey(lambda v1,v2: v1 + v2).sortBy(lambda a: a[1], ascending=False)
 
-counts.saveAsTextFile("SparkAnalyticOutput")
+countRDD = inputsRDD.count()
+
+normalizeCounts = counts.map(lambda (x, y): (x, y/float(countRDD)))
+
+normalizeCounts.saveAsTextFile("SparkAnalyticOutput")
